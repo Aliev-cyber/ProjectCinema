93a52583694745b0ac8e3606409171ef2795fce7
ixelin <radfem54@gmail.com>
2023-07-19 17:51:47 +0600

    done

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,54 +4,9 @@ import MovieItem from "./MovieItem";
 import { Box, CircularProgress, TextField, Slider, Button } from "@mui/material";
 
 const MovieList = () => {
-  const { getMovie, movies, search, setSearch, setRating, rating } =
+  const { movies } =
     useMovieContext();
-  console.log(movies);
-
-  useEffect(() => {
-    getMovie();
-  }, [search, rating]);
-
-  const handleSearchChange = (event) => {
-    setSearch(event.target.value);
-  };
-  const handleRatingChange = (event) => {
-    setRating(event.target.value);
-  };
-  const handleResetRating = () => {
-    setRating(null);
-  };
   return (
-    <Box>
-      <TextField
-        label="Search Movie"
-        variant="outlined"
-        value={search}
-        onChange={handleSearchChange}
-        sx={{
-          margin: "3rem",
-          width: "100",
-          display: "block",
-          "> .MuiOutlinedInput-root": {
-            display: "block",
-            width: "25rem",
-          },
-        }}
-      />
-      <Slider
-        value={rating}
-        onChange={handleRatingChange}
-        min={1}
-        max={5}
-        step={1}
-        marks
-        valueLabelDisplay="auto"
-        sx={{ margin: "3rem", width: "40vw" }}
-      />
-      <Button variant="outlined" onClick={handleResetRating} sx={{ margin: "3rem", width: "10vw" }}>
-        All Movies
-      </Button>
-
       <Box
         sx={{
           display: "flex",
@@ -68,7 +23,6 @@ const MovieList = () => {
           />
         )}
       </Box>
-    </Box>
   );
 };
 
