6843374075d5990164e25ea8d85fb0bfca7ae84c
Aya-Taitelieva <aya.taitelieva@gmail.com>
2023-07-19 17:54:46 +0300

    fix bag

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -102,13 +102,7 @@ export default function MovieItem({ item }) {
       <CardContent>
         <Typography variant="h6">{item.authors}</Typography>
         <Typography variant="body2" color="text.secondary">
-          <Rating
-            name="simple-controlled"
-            value={formValue.rating}
-            onChange={(event, newValue) => {
-              setFormValue({ ...formValue, rating: newValue });
-            }}
-          />
+          <Rating name="simple-controlled" value={formValue.rating} />
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
